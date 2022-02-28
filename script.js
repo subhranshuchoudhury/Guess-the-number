8b29a989a650773d9e4dd4937edddf926ec7a262
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-02-28 20:44:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const originalnum = Math.floor(Math.random() * 10) + 1;
-var counter = 4;
+var counter = 0;
+var enter_sound = new Audio('sounds/pop.mp3');
+var win_sound = new Audio('sounds/win.mp3');
+var lose_sound = new Audio('sounds/lose.mp3');
 document.getElementById("win_display").style.display = "none";
 function guess() {
+enter_sound.play();
+enter_sound.currentTime = 0;
 var guessnum = document.getElementById('input_num').value;
-
+counter++;
 
 //if guess is smaller.
 if (guessnum < originalnum) {
@@ -14,21 +19,23 @@ else if (guessnum > originalnum) {
 document.getElementById('display').innerHTML = "Your Guess Is Large &#128530;";
 
 }
-if (guessnum == originalnum) {
+else if (guessnum == originalnum) {
+win_sound.play();
+win_sound.currentTime = 0;
 document.getElementById('display').innerHTML = "You Won!";
 document.getElementById("win_display").style.display = "";
 
 }
-else{
-counter--;
-}
-document.getElementById('total_guess').innerHTML = "Guess Left: " + counter;
-if (counter == 0) {
+document.getElementById('total_guess').innerHTML = "Total Guessed: " + counter + "/5";
+if (counter == 5) {
+lose_sound.play();
 alert("Game Over! You Lose :(");
 location.reload()
 }
 }
 function restartgame(argument) {
+enter_sound.play();
+enter_sound.currentTime = 0;
 location.reload()
 // body...
 }
